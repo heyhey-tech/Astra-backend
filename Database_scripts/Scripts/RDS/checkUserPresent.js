aeb012950cdac74309802897892cb948ef3eb93d
heyaasif <aasif.javid@heyheyglobal.com>
2023-10-30 12:45:19 +0530

    .

--- Database_scripts/Scripts/RDS/checkUserPresent.js
+++ Database_scripts/Scripts/RDS/checkUserPresent.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 require('dotenv').config({ path: '.env'});
 
 const rdspw = process.env.RDS;
+
 function checkUserInDB(email) {
   const connection = mysql.createConnection({
     host: 'project-astra-rds.c5y9t5m5qhwe.ap-south-1.rds.amazonaws.com',
