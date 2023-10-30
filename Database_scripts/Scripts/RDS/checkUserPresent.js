8e9cf2efd0e70d45ab499991acda1772a7196b21
heyaasif <aasif.javid@heyheyglobal.com>
2023-10-30 12:47:14 +0530

    ..

--- Database_scripts/Scripts/RDS/checkUserPresent.js
+++ Database_scripts/Scripts/RDS/checkUserPresent.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
+require('dotenv').config({ path: '.env'});
+
+const rdspw = process.env.RDS;
 
 function checkUserInDB(email) {
   const connection = mysql.createConnection({
     host: 'project-astra-rds.c5y9t5m5qhwe.ap-south-1.rds.amazonaws.com',
     user: 'admin',
-    password: 'pw',
+    password: rdspw,
     database: 'astraDB',
   });
 
--- Scripts/addTemp.js
+++ Scripts/addTemp.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
+require('dotenv').config({ path: '.env'});
+
+const rdspw = process.env.RDS;
 
 function addVerifyCodeToRDS(email, code,_password) {
     console.log('here:password is:',_password);
   const connection = mysql.createConnection({
     host: 'project-astra-rds.c5y9t5m5qhwe.ap-south-1.rds.amazonaws.com',
     user: 'admin',
-    password: 'pw',
+    password: rdspw,
     database: 'astraDB',
   });
 
